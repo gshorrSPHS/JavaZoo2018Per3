e005d066a261da528f07fa6c9e04afb23050a706
ameliaranthony <37593228+ameliaranthony@users.noreply.github.com>
2018-03-22 11:19:32 -0700

    Amelia anthony

---
+++ MagicBushBaby.java
@@ -0,0 +1,14 @@
+public class MagicBushBaby extends BushBaby
+{
+    public MagicBushBaby()
+    {
+        super("Magic Bush Baby", "A bush baby... but make it magic" , 100, 1000);
+    }
+    
+    public String doMagic()
+    {
+        return "the bush baby would be happy to perform some magic for you \n but the bush baby is nocturnal. \n please try again later.";
+    }
+    
+    
+}
