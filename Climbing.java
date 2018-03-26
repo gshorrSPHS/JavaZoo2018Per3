5bbccf58b1ddceff1451879bf39d3691e60bd89d
nicoleksri <37669528+nicoleksri@users.noreply.github.com>
2018-03-26 11:25:58 -0700

    Add files via upload

---
+++ Climbing.java
@@ -0,0 +1,6 @@
+public interface Climbing 
+{
+    //by default, all methods in interfaces are abstract and public 
+    //so you don't have to write out those keywords
+    String climb();
+}
---
+++ Sleeping.java
@@ -0,0 +1,6 @@
+public interface Sleeping 
+{
+    //by default, all methods in interfaces are abstract and public 
+    //so you don't have to write out those keywords
+    String sleep();
+}
