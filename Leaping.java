e53c5e1997dcbc0b5e0ce5b6e5e8d6535969a403
audreyisabel <37467517+audreyisabel@users.noreply.github.com>
2018-03-26 11:26:51 -0700

    Add files via upload

---
+++ Leaping.java
@@ -0,0 +1,4 @@
+public interface Leaping
+{
+    String leap();
+}
--- Sleeping.java
+++ Sleeping.java
@@ -1,6 +1,4 @@
-public interface Sleeping 
+public interface Sleeping
 {
-    //by default, all methods in interfaces are abstract and public 
-    //so you don't have to write out those keywords
     String sleep();
 }
